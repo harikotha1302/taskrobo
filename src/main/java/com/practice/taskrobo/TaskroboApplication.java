c729259e65ec91fce18927ae61542ee7adf5dcd5
Harikrishna <harikrishnakotha13@gmail.com>
2020-06-11 15:04:54 +0530

    initial commit

---
+++ src/main/java/com/practice/taskrobo/TaskroboApplication.java
@@ -0,0 +1,13 @@
+package com.practice.taskrobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskroboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskroboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/taskrobo/TaskroboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.taskrobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskroboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
